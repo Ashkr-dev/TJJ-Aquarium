1c61c0c07752c8fd75f8535a3e46105dd9522da7
Ashkr-dev <ashkr2657@gmail.com>
2025-08-21 17:43:27 +0530

    prepare for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
+import { defineConfig } from 'vite'
 import restart from 'vite-plugin-restart'
 import glsl from 'vite-plugin-glsl'
 
-export default {
-    root: '.', // <-- changed from 'src/'
-    publicDir: 'static',
+export default defineConfig({
+    root: 'src/',
+    publicDir: '../static/',
     base: './',
     server: {
         host: true,
         open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env)
     },
     build: {
-        outDir: 'dist',
+        outDir: '../dist',
         emptyOutDir: true,
         sourcemap: true
     },
     plugins: [
-        restart({ restart: ['static/**'] }),
+        restart({ restart: ['../static/**'] }),
         glsl()
     ]
-}
+})
