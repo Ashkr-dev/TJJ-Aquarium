f0c5faf6007d01e077cde8fd8cb80bbc71928e12
Ashkr-dev <ashkr2657@gmail.com>
2025-08-21 17:23:28 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,21 @@
-import { defineConfig } from 'vite'
 import restart from 'vite-plugin-restart'
 import glsl from 'vite-plugin-glsl'
 
-export default defineConfig({
-    root: 'src/',
-    publicDir: '../static/',
+export default {
+    root: '.', // <-- changed from 'src/'
+    publicDir: 'static',
     base: './',
     server: {
         host: true,
         open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env)
     },
     build: {
-        outDir: '../dist',
+        outDir: 'dist',
         emptyOutDir: true,
         sourcemap: true
     },
     plugins: [
-        restart({ restart: ['../static/**'] }),
+        restart({ restart: ['static/**'] }),
         glsl()
     ]
-})
+}
