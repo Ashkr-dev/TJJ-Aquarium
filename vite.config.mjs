266847a43723ff66d53df7e4c42b7d7befd53a82
Ashkr-dev <ashkr2657@gmail.com>
2025-08-21 21:25:33 +0530

    fixed once again after deleting files

---
+++ vite.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import restart from 'vite-plugin-restart';
+import glsl from 'vite-plugin-glsl';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: 'src/',
+  publicDir: '../static/',
+  base: './',
+  server: {
+    host: true,
+    open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env),
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+  plugins: [
+    restart({ restart: ['../static/**'] }),
+    glsl(),
+  ],
+});
